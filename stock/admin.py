2a503de08a7eff069b7c9e5b4e5a102c712555e1
Malinin_Kirill <kirisumal@gmail.com>
2024-01-24 00:52:36 +0300

    Lab 6

--- stock/admin.py
+++ stock/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Stock, Currency
+from .models import Stock, Currency, Account, AccountCurrency, AccountStock
 
 @admin.register(Stock)
 class StockAdmin(admin.ModelAdmin):
@@ -11,4 +11,23 @@ class CurrencyAdmin(admin.ModelAdmin):
     pass
     # Вы можете добавить дополнительные настройки для Currency, если это необходимо
 
+@admin.register(AccountCurrency)
+class AccountCurrencyAdmin(admin.ModelAdmin):
+    pass
+    # Дополнительные настройки для AccountCurrency, если это необходимо
 
+@admin.register(AccountStock)
+class AccountStockAdmin(admin.ModelAdmin):
+    pass
+    # Дополнительные настройки для AccountStock, если это необходимо
+
+class AccountCurrencyInline(admin.TabularInline):
+    model = AccountCurrency
+
+class AccountStockInline(admin.TabularInline):
+    model = AccountStock
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    inlines = [AccountCurrencyInline, AccountStockInline]
+    # Дополнительные настройки для Account, если это необходимо
